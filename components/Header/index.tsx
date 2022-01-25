067ab721b10513b4450906285f001c55f6271a54
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-01-25 19:16:16 +0000

    bg divider

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,11 +4,13 @@ import styles from './style.module.scss'
 export function Header() {
     const Title = "Dev Oliveira"
     return (
-        <header className={styles.container}>
-            <div className={styles.logo}>                                    
-                <h2> {"<"}{Title}<span>{"/>"}</span> </h2> 
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <div className={styles.logo}>                                    
+                    <h2> {"<"}{Title}<span>{"/>"}</span> </h2> 
+                </div>
+                <Navbar/>
             </div>
-            <Navbar/>
         </header>
     )
 }
