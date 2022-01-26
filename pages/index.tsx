e1812e4b702f8523304f41689bd9499bd48826ef
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-01-26 18:42:04 +0000

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           Weslley Oliveira
         </h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Hello there text of the printing and typesetting industry.
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         </p>
         <button>Download CV</button>        
