ded821ed0fa7f9b1f63ea9daa884073ef711546b
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-01-28 01:57:51 +0000

    menu changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import { Navbar } from '../Navbar'
 import styles from './style.module.scss'
 
 export function Header() {
-    const Title = "Dev Oliveira"
-    return (
+    const Title = "Oliveira"
+
+    const [isToggled, setIsToggled] = useState(false)
+
+    return (<>
         <header className={styles.header}>
-            <div className={styles.container}>
-                <div className={styles.logo}>                                    
-                    <h2> {"<"}{Title}<span>{"/>"}</span> </h2> 
-                </div>
-                <Navbar/>
+            <div className={styles.title}>
+                <h2> {"<"}{Title}<span>{"/>"}</span> </h2>
             </div>
+            <div
+                onClick={()=>setIsToggled(!isToggled)} 
+                className={`${styles.menuBars}  ${isToggled?styles.active:''}`}>
+                    
+                <div className={styles.line1}></div>
+                <div className={styles.line2}></div>
+                <div className={styles.line3}></div>
+            </div>           
         </header>
-    )
+        <Navbar button={isToggled}/>
+    </>)
 }
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,64 +5,42 @@ import { BiHomeAlt } from 'react-icons/bi';
 import { FaLaptopCode } from 'react-icons/fa';
 import { GiTechnoHeart } from 'react-icons/gi';
 
-import Lottie from 'react-lottie';
-import animationData from './codeAnimation.json';
-
 import { Link } from 'react-scroll'
 
-export function Navbar() {
+interface buttonProps{
+    button: boolean
+ }
 
-    const [isToggled, setIsToggled] = useState(false) 
-    
-    const [animationState, setAnimationState] = useState({
-        isStopped: false, isPaused: false,
-      });
-    
-      const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
-        }
-      };
+export function Navbar({ button }: buttonProps) {
 
+   const isToggled =  button;
+        console.log(isToggled)
     return(
-        <nav>
-            <div 
-                onClick={()=>setIsToggled(!isToggled)} 
-                className={`${style.mobileMenu}  ${isToggled?style.active:''}`}
-            >
-                <div className={style.line1}></div>
-                <div className={style.line2}></div>
-                <div className={style.line3}></div>
-            </div>          
-            
+        <nav>                   
             <div className={`${style.listMenu}  ${isToggled?style.active:''}`}>
                 <ul >
                     <li>
                         <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500} >
-                        <a href="#" onClick={()=>setIsToggled(!isToggled)}><span><BiHomeAlt/></span>Home</a>
+                            <span><BiHomeAlt/></span>Home
                         </Link>
                     </li>
                     <li>
                         <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500} >
-                            <a href="#" onClick={()=>setIsToggled(!isToggled)}><span><BsFillPersonLinesFill/></span> Who Am I </a>
+                            <span><BsFillPersonLinesFill/></span> Who Am I
                         </Link>
                     </li> 
                     <li>
                         <Link activeClass="active" to="projects" spy={true} smooth={true} offset={50} duration={500} >
-                            <a href="#" onClick={()=>setIsToggled(!isToggled)}><span><FaLaptopCode/></span>Projects</a>
+                           <span><FaLaptopCode/></span>Projects
                         </Link>
                     </li>
                     <li>
                         <Link activeClass="active" to="technologies" spy={true} smooth={true} offset={50} duration={500}  >
-                            <a href="#" onClick={()=>setIsToggled(!isToggled)}><span><GiTechnoHeart/></span>Tecnologies</a>
+                            <span><GiTechnoHeart/></span>Tecnologies
                         </Link>
                     </li>
                 </ul>                
-            </div>
-                        
+            </div>                        
         </nav>
     )
 
