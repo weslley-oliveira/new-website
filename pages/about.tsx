726483ff488bfbe8d4fe3614fd82437e9f837ffe
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-01-29 14:11:57 +0000

    about me start

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Projects.module.scss'
+import styles from '../styles/About.module.scss'
 
+import {  BsGithub } from 'react-icons/bs';
+import {  BsLinkedin } from 'react-icons/bs';
+import {  BsInstagram } from 'react-icons/bs';
 
 const About: NextPage = () => {
   return (
@@ -8,13 +11,28 @@ const About: NextPage = () => {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-          Who Am I
+          Who Am I <span>?</span>
         </h1>
 
         <p className={styles.description}>
-          Welcome to my website
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident
         </p>
-
+        
+        <div className={styles.social}>
+          <div className={styles.github}>
+            <BsGithub/> <span>weslley-oliveira</span>
+          </div>
+          <div className={styles.linkedin}>
+            <BsLinkedin/> <span>weslley-oliveira-uk</span>
+          </div>
+          <div className={styles.instagram}>
+            <BsInstagram/> <span>weslley-instagram</span>
+          </div>          
+        </div>
        
       </main>
 
