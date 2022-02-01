08f642975ae7c6a3037a2cd5858017dfcfdca3dc
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-02-01 20:17:20 +0000

    voltis

--- pages/about.tsx
+++ pages/about.tsx
@@ -49,11 +49,11 @@ const About: NextPage = () => {
         </div>
        
       </main>
-      {/* <div className={styles.dividerAbout}>
+      <div className={styles.dividerAbout}>
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z" className={styles.shapeFill}></path>
           </svg>
-      </div> */}
+      </div>
 
     </div>
     
